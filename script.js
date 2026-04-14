ea69b6d71104795ff45ac5e8bc3271a3107a7873
stillwild <gamesrun2211@gmail.com>
2026-04-14 20:30:05 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const CFG = {
 
     // Вебхук куда приходит итог (принят / отклонён) после action.html
     // Можно использовать тот же канал что и для заявок
-    resultWebhook: https://discord.com/api/webhooks/1493634222399950971/DXVr8vANP2_MgDvOkzuRAbHO384CFudqFMWfUg4Q_5Poxmdd85tiEnyur-6mgZOLBPG8',
+    resultWebhook: 'https://discord.com/api/webhooks/1493634222399950971/DXVr8vANP2_MgDvOkzuRAbHO384CFudqFMWfUg4Q_5Poxmdd85tiEnyur-6mgZOLBPG8',
 
     // Секретный ключ для action.html — можно любое слово
     // Нужен чтобы чужие не могли открыть action.html и кликать
